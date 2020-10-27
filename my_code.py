b205511c108e61922e337410add30aa89384ce94
aaravaggarwal <24aaggarwal@athenian.org>
2020-10-27 09:26:25 -0700

    Python Day 1

--- my_code.py
+++ my_code.py
@@ -1,3 +1,11 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
 
+print('Hi Megan')
+print("Hello World")
+
+name="aarav"
+print(f'Hello {name}')
+hello= 'hello'
+print(f'{hello} {name}')
+
